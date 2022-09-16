beae29ff2ace66eb5fe8c5dd4022d74767f08a56
vbidin <vedran@bidin.me>
2022-09-16 18:26:34 +0300

    test: Add failure mode integration tests (SC-8406) (#56)
    * fix: update sumbodules and tests
    
    * fix: update formatting
    
    * fix: update liquidations submodule
    
    * fix: add loan impairment updates
    
    * fix: remove late interest from unrealized losses
    
    * fix: update formatting
    
    * test: add failure tests for td and fcl
    
    Co-authored-by: lucas-manuel <lucas@maple.finance>

---
+++ tests/TriggerDefault.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.7;
+
+import { TestBaseWithAssertions } from "../contracts/utilities/TestBaseWithAssertions.sol";
+
+import { Address, console  } from "../modules/contract-test-utils/contracts/test.sol";
+import { MapleLoan as Loan } from "../modules/loan/contracts/MapleLoan.sol";
+
+contract TriggerDefaultTests is TestBaseWithAssertions {
+
+    Loan loan;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        depositLiquidity({
+            lp: address(new Address()),
+            liquidity: 1_500_000e6
+        });
+
+        setupFees({
+            delegateOriginationFee:     500e6,
+            delegateServiceFee:         275e6,
+            delegateManagementFeeRate:  0.02e6,
+            platformOriginationFeeRate: 0.001e6,
+            platformServiceFeeRate:     0.0066e6,
+            platformManagementFeeRate:  0.08e6
+        });
+
+        loan = fundAndDrawdownLoan({
+            borrower:    address(new Address()),
+            termDetails: [uint256(5 days), uint256(30 days), uint256(3)],
+            amounts:     [uint256(0), uint256(1_000_000e6), uint256(1_000_000e6)],
+            rates:       [uint256(0.075e18), uint256(0), uint256(0), uint256(0)]
+        });
+    }
+
+    function test_triggerDefault_notAuthorized() external {
+        vm.expectRevert("PM:TD:NOT_AUTHORIZED");
+        poolManager.triggerDefault(address(loan), address(liquidatorFactory));
+    }
+
+    function test_triggerDefault_notFactory() external {
+        vm.prank(address(poolDelegate));
+        vm.expectRevert("PM:TD:NOT_FACTORY");
+        poolManager.triggerDefault(address(loan), address(1));
+    }
+
+    function test_triggerDefault_notPoolManager() external {
+        vm.prank(address(loanManager));
+        vm.expectRevert("LM:TD:NOT_PM");
+        loanManager.triggerDefault(address(loan), address(liquidatorFactory));
+    }
+
+}
