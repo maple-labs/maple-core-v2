6287136fa0784c2bf1231144bdd409e7168d18d0
Lucas Manuel <lucasmanuel.tech@gmail.com>
2022-09-21 12:52:32 -0400

    feat: Add deploy pool success test (SC-8599) (#59)
    * feat: add deploy pool success test
    
    * feat: merge main

--- tests/DeployPool.t.sol
+++ tests/DeployPool.t.sol
@@ -7,7 +7,13 @@ import { Address, console } from "../modules/contract-test-utils/contracts/test.
 
 import { IMapleProxyFactory } from "../modules/pool-v2/modules/maple-proxy-factory/contracts/interfaces/IMapleProxyFactory.sol";
 
-contract DeployPoolFailureTests is TestBaseWithAssertions {
+import { ILoanManager } from "../modules/pool-v2/contracts/interfaces/ILoanManager.sol";
+import { IPool        } from "../modules/pool-v2/contracts/interfaces/IPool.sol";
+import { IPoolManager } from "../modules/pool-v2/contracts/interfaces/IPoolManager.sol";
+
+import { IWithdrawalManager } from "../modules/withdrawal-manager/contracts/interfaces/IWithdrawalManager.sol";
+
+contract DeployPoolTests is TestBaseWithAssertions {
 
     function setUp() public override {
         _createAccounts();
@@ -215,6 +221,69 @@ contract DeployPoolFailureTests is TestBaseWithAssertions {
     function test_deployPool_failIfCalledWMFactoryDirectly() external {
         vm.expectRevert("WMF:CI:NOT_DEPLOYER");
         IMapleProxyFactory(withdrawalManagerFactory).createInstance(new bytes(0), "salt");
+    }
+
+    function test_deployPool_success() external {
+        fundsAsset.mint(poolDelegate, 1_000_000e6);
+
+        vm.startPrank(poolDelegate);
+
+        fundsAsset.approve(address(deployer), 1_000_000e6);
+
+        ( address poolManager_, address loanManager_, address withdrawalManager_) = deployer.deployPool({
+            factories_:    [poolManagerFactory,     loanManagerFactory,     withdrawalManagerFactory],
+            initializers_: [poolManagerInitializer, loanManagerInitializer, withdrawalManagerInitializer],
+            asset_:        address(fundsAsset),
+            name_:         "Maple Pool",
+            symbol_:       "MP",
+            configParams_: [uint256(1_500_000e6), 0.2e6, 1_000_000e6, 1 weeks, 2 days, 2_000_000e6]
+        });
+
+        IPoolManager       poolManager       = IPoolManager(poolManager_);
+        IPool              pool              = IPool(poolManager.pool());
+        ILoanManager       loanManager       = ILoanManager(loanManager_);
+        IWithdrawalManager withdrawalManager = IWithdrawalManager(withdrawalManager_);
+
+        assertEq(poolManager.poolDelegate(),              poolDelegate);
+        assertEq(poolManager.asset(),                     address(fundsAsset));
+        assertEq(poolManager.withdrawalManager(),         withdrawalManager_);
+        assertEq(poolManager.liquidityCap(),              1_500_000e6);
+        assertEq(poolManager.delegateManagementFeeRate(), 0.2e6);
+        assertEq(poolManager.loanManagerList(0),          loanManager_);
+
+        assertTrue(poolManager.configured());
+        assertTrue(poolManager.isLoanManager(loanManager_));
+        assertTrue(poolManager.pool().code.length              > 0);
+        assertTrue(poolManager.poolDelegateCover().code.length > 0);
+
+        assertEq(pool.name(),                              "Maple Pool");
+        assertEq(pool.symbol(),                            "MP");
+        assertEq(pool.asset(),                             address(fundsAsset));
+        assertEq(pool.manager(),                           poolManager_);
+        assertEq(pool.totalSupply(),                       2_000_000e6);
+        assertEq(pool.balanceOf(globals.migrationAdmin()), 2_000_000e6);
+
+        assertEq(fundsAsset.allowance(address(pool), poolManager_),     type(uint256).max);
+        assertEq(fundsAsset.balanceOf(poolManager.poolDelegateCover()), 1_000_000e6);
+
+        assertEq(loanManager.pool(),        address(pool));
+        assertEq(loanManager.fundsAsset(),  address(fundsAsset));
+        assertEq(loanManager.poolManager(), poolManager_);
+
+        assertEq(withdrawalManager.pool(),        address(pool));
+        assertEq(withdrawalManager.poolManager(), poolManager_);
+
+        (
+            uint64 initialCycleId_,
+            uint64 initialCycleTime_,
+            uint64 cycleDuration_,
+            uint64 windowDuration_
+        ) = withdrawalManager.cycleConfigs(0);
+
+        assertEq(initialCycleId_,   1);
+        assertEq(initialCycleTime_, block.timestamp);
+        assertEq(cycleDuration_,    1 weeks);
+        assertEq(windowDuration_,   2 days);
     }
 
 }
