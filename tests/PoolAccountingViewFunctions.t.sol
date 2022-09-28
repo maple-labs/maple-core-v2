f29cb275bef95025a9e17c3ce3d5c3cf27b14f1f
Farhaan <59924029+0xfarhaan@users.noreply.github.com>
2022-09-28 12:02:46 +0100

    feat: Add integration tests for remaining view functions in PoolAccountingViewFunctions.sol (SC-8604) (#66)
    * chore: update wm sub-module
    
    * feat: Tests for maxRedeem and maxWithdraw
    
    * feat: previewRedeem tests
    
    * feat: previewWithdraw tests
    
    * style: Fix formatting
    
    * feat: convertToAssets and convertToShares tests
    
    * feat: previewDeposit tests
    
    * feat: previewMint tests
    
    * feat: totalAssets tests
    
    * style: formatting changes
    
    * style: formatting changes
    
    * refactor: update all depositLiquity calls in PR scope and format
    
    * feat: update assertions as requested in PR
    
    * feat: Add missing assertion for shares variable
    
    * style: sentence casing, spacing etc
    
    * feat: add time warp to tests as suggested
    
    * style: fix styling for -1
    
    * chore: add todo for fuzzing
    
    Co-authored-by: lucas-manuel <lucas@maple.finance>

--- tests/PoolAccountingViewFunctions.t.sol
+++ tests/PoolAccountingViewFunctions.t.sol
@@ -72,6 +72,8 @@ contract BalanceOfAssetsTests is TestBase {
 
 }
 
+// TODO: Add fuzz tests for all view function success cases.
+
 contract MaxDepositTests is TestBase {
 
     address lp1;
@@ -245,6 +247,7 @@ contract MaxMintTests is TestBase {
         poolManager.setOpenToPublic();
         vm.stopPrank();
 
+        // TODO: Change all depositLiquidity calls to use depositLiquidity(lp1, 1_000e6) pattern.
         depositLiquidity({
             lp:        lp1,
             liquidity: depositAmount
@@ -266,12 +269,536 @@ contract MaxMintTests is TestBase {
     }
 }
 
-contract MaxRedeemTests is TestBase {}
-contract MaxWithdrawTests is TestBase {}
-contract PreviewRedeemTests is TestBase {}
-contract PreviewWithdrawTests is TestBase {}
-contract ConvertToAssetsTests is TestBase {}
-contract ConvertToSharesTests is TestBase {}
-contract PreviewDepositTests is TestBase {}
-contract PreviewMintTests is TestBase {}
-contract TotalAssetsTests is TestBase {}
+contract MaxRedeemTests is TestBase {
+
+    address lp1;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+    }
+
+    function test_maxRedeem_noLockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.balanceOf(lp1), 1_000e6);
+
+        assertTrue(!withdrawalManager.isInExitWindow(lp1));
+
+        assertEq(pool.maxRedeem(lp1), 0);
+    }
+
+    function test_maxRedeem_lockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.balanceOf(lp1), 1_000e6);
+
+        vm.prank(lp1);
+        pool.requestRedeem(1_000e6);
+
+        vm.warp(start + 2 weeks - 1);
+
+        assertTrue(!withdrawalManager.isInExitWindow(lp1));
+
+        assertEq(pool.maxRedeem(lp1), 0);
+    }
+
+    function test_maxRedeem_lockedShares_inExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.balanceOf(lp1), 1_000e6);
+
+        vm.prank(lp1);
+        uint256 shares = pool.requestRedeem(1_000e6);
+
+        assertEq(shares, 1_000e6);
+        assertEq(pool.maxRedeem(lp1), 0);
+
+        vm.warp(start + 2 weeks);
+
+        assertTrue(withdrawalManager.isInExitWindow(lp1));
+
+        assertEq(pool.maxRedeem(lp1), shares);
+    }
+
+}
+
+contract MaxWithdrawTests is TestBase {
+
+    address lp1;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+    }
+
+    function test_maxWithdraw_noLockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.balanceOf(lp1), 1_000e6);
+
+        assertTrue(!withdrawalManager.isInExitWindow(lp1));
+
+        assertEq(pool.maxWithdraw(lp1), 0);
+    }
+
+    function test_maxWithdraw_lockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.balanceOf(lp1), 1_000e6);
+
+        vm.prank(lp1);
+        pool.requestWithdraw(1_000e6);
+
+        vm.warp(start + 2 weeks - 1);
+
+        assertTrue(!withdrawalManager.isInExitWindow(lp1));
+
+        assertEq(pool.maxWithdraw(lp1), 0);
+    }
+
+    function test_maxWithdraw_lockedShares_inExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.balanceOf(lp1), 1_000e6);
+
+        vm.prank(lp1);
+        pool.requestWithdraw(1_000e6);
+
+        assertEq(pool.maxWithdraw(lp1), 0);
+
+        vm.warp(start + 2 weeks);
+
+        assertTrue(withdrawalManager.isInExitWindow(lp1));
+
+        assertEq(pool.maxWithdraw(lp1), 1_000e6);
+    }
+
+}
+
+contract PreviewRedeemTests is TestBase {
+
+    address lp1;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+    }
+
+    function test_previewRedeem_invalidShares() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.startPrank(lp1);
+        pool.requestRedeem(1_000e6);
+
+        vm.expectRevert("WM:PE:INVALID_SHARES");
+        pool.previewRedeem(1);
+    }
+
+    function test_previewRedeem_noLockedShares_notInExitWindow() external {
+        vm.prank(lp1);
+        vm.expectRevert("WM:PR:NO_REQUEST");
+        pool.previewRedeem(0);
+    }
+
+    function test_previewRedeem_lockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.startPrank(lp1);
+        pool.requestRedeem(1_000e6);
+
+        vm.warp(start + 2 weeks - 1);
+
+        vm.expectRevert("WM:PR:NOT_IN_WINDOW");
+        pool.previewRedeem(1_000e6);
+    }
+
+    function test_previewRedeem_lockedShares_inExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.startPrank(lp1);
+        pool.requestRedeem(1_000e6);
+
+        vm.warp(start + 2 weeks);
+
+        assertEq(pool.previewRedeem(1_000e6), 1_000e6);
+    }
+
+}
+
+contract PreviewWithdrawTests is TestBase {
+
+    address lp1;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+    }
+
+    function test_previewWithdraw_invalidShares() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.startPrank(lp1);
+        pool.requestWithdraw(1_000e6);
+
+        vm.expectRevert("WM:PE:INVALID_SHARES");
+        pool.previewWithdraw(1);
+    }
+
+    function test_previewWithdraw_noLockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.prank(lp1);
+        vm.expectRevert("WM:PR:NO_REQUEST");
+        pool.previewWithdraw(0);
+    }
+
+    function test_previewWithdraw_lockedShares_notInExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.startPrank(lp1);
+        pool.requestWithdraw(1_000e6);
+
+        vm.warp(start + 2 weeks - 1);
+
+        vm.expectRevert("WM:PR:NOT_IN_WINDOW");
+        pool.previewWithdraw(1_000e6);
+    }
+
+    function test_previewWithdraw_lockedShares_inExitWindow() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        vm.startPrank(lp1);
+        pool.requestWithdraw(1_000e6);
+
+        vm.warp(start + 2 weeks);
+
+        assertEq(pool.previewWithdraw(1_000e6), 1_000e6);
+    }
+
+}
+
+contract ConvertToAssetsTests is TestBase {
+
+    address lp1;
+    address lp2;
+    address lp3;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+        lp2 = address(new Address());
+        lp3 = address(new Address());
+    }
+
+    function test_convertToAssets_zeroTotalSupply() external {
+        assertEq(pool.convertToAssets(1),       1);
+        assertEq(pool.convertToAssets(2),       2);
+        assertEq(pool.convertToAssets(1_000e6), 1_000e6);
+    }
+
+    function test_convertToAssets_singleUser() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.convertToAssets(1),       1);
+        assertEq(pool.convertToAssets(2),       2);
+        assertEq(pool.convertToAssets(1_000e6), 1_000e6);
+    }
+
+    function test_convertToAssets_multipleUsers() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        assertEq(pool.convertToAssets(1),       1);
+        assertEq(pool.convertToAssets(2),       2);
+        assertEq(pool.convertToAssets(1_000e6), 1_000e6);
+    }
+
+    function test_convertToAssets_multipleUsers_changeTotalAssets() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        vm.prank(address(pool));
+        fundsAsset.transfer(address(0), 1_500e6);  // Simulate loss of 50% of funds
+
+        assertEq(pool.convertToAssets(1),       0);  // Rounds down as expected
+        assertEq(pool.convertToAssets(2),       1);
+        assertEq(pool.convertToAssets(1_000e6), 500e6);
+    }
+
+}
+
+contract ConvertToSharesTests is TestBase {
+
+    address lp1;
+    address lp2;
+    address lp3;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+        lp2 = address(new Address());
+        lp3 = address(new Address());
+    }
+
+    function test_convertToShares_zeroTotalSupply() external {
+        assertEq(pool.convertToShares(1),       1);
+        assertEq(pool.convertToShares(2),       2);
+        assertEq(pool.convertToShares(1_000e6), 1_000e6);
+    }
+
+    function test_convertToShares_singleUser() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.convertToShares(1),       1);
+        assertEq(pool.convertToShares(2),       2);
+        assertEq(pool.convertToShares(1_000e6), 1_000e6);
+    }
+
+    function test_convertToShares_multipleUsers() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        assertEq(pool.convertToShares(1),       1);
+        assertEq(pool.convertToShares(2),       2);
+        assertEq(pool.convertToShares(1_000e6), 1_000e6);
+    }
+
+    function test_convertToShares_multipleUsers_changeTotalAssets() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        vm.prank(address(pool));
+        fundsAsset.transfer(address(0), 1_500e6);  // Simulate loss of 50% of funds
+
+        assertEq(pool.convertToShares(1),       2);
+        assertEq(pool.convertToShares(2),       4);
+        assertEq(pool.convertToShares(1_000e6), 2_000e6);
+    }
+
+}
+
+contract PreviewDepositTests is TestBase {
+
+    address lp1;
+    address lp2;
+    address lp3;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+        lp2 = address(new Address());
+        lp3 = address(new Address());
+    }
+
+    function test_previewDeposit_zeroTotalSupply() external {
+        assertEq(pool.previewDeposit(1),       1);
+        assertEq(pool.previewDeposit(2),       2);
+        assertEq(pool.previewDeposit(1_000e6), 1_000e6);
+    }
+
+    function test_previewDeposit_nonZeroTotalSupply() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.previewDeposit(1),       1);
+        assertEq(pool.previewDeposit(2),       2);
+        assertEq(pool.previewDeposit(1_000e6), 1_000e6);
+    }
+
+    function test_previewDeposit_multipleUsers() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        assertEq(pool.previewDeposit(1),       1);
+        assertEq(pool.previewDeposit(2),       2);
+        assertEq(pool.previewDeposit(1_000e6), 1_000e6);
+    }
+
+    function test_previewDeposit_multipleUsers_changeTotalAssets() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        vm.prank(address(pool));
+        fundsAsset.transfer(address(0), 1_500e6);  // Simulate loss of 50% of funds
+
+        assertEq(pool.previewDeposit(1),       2);
+        assertEq(pool.previewDeposit(2),       4);
+        assertEq(pool.previewDeposit(1_000e6), 2_000e6);
+    }
+
+}
+
+contract PreviewMintTests is TestBase {
+
+    address lp1;
+    address lp2;
+    address lp3;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1 = address(new Address());
+        lp2 = address(new Address());
+        lp3 = address(new Address());
+    }
+
+    function test_previewMint_zeroTotalSupply() external {
+        assertEq(pool.previewMint(1),       1);
+        assertEq(pool.previewMint(2),       2);
+        assertEq(pool.previewMint(1_000e6), 1_000e6);
+    }
+
+    function test_previewMint_nonZeroTotalSupply() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.previewMint(1),       1);
+        assertEq(pool.previewMint(2),       2);
+        assertEq(pool.previewMint(1_000e6), 1_000e6);
+    }
+
+    function test_previewMint_multipleUsers() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        assertEq(pool.previewMint(1),       1);
+        assertEq(pool.previewMint(2),       2);
+        assertEq(pool.previewMint(1_000e6), 1_000e6);
+    }
+
+    function test_previewMint_multipleUsers_changeTotalAssets() external {
+        depositLiquidity(lp1, 1_000e6);
+        depositLiquidity(lp2, 1_000e6);
+        depositLiquidity(lp3, 1_000e6);
+
+        vm.prank(address(pool));
+        fundsAsset.transfer(address(0), 1_500e6);  // Simulate loss of 50% of funds
+
+        assertEq(pool.previewMint(1),       1);  // Rounds up the asset token amount
+        assertEq(pool.previewMint(2),       1);
+        assertEq(pool.previewMint(1_000e6), 500e6);
+    }
+
+}
+
+contract TotalAssetsTests is TestBase {
+
+    address lp1;
+    address borrower;
+    Loan loan;
+
+    function setUp() public override {
+        super.setUp();
+
+        lp1      = address(new Address());
+        borrower = address(new Address());
+
+        vm.prank(governor);
+        globals.setValidBorrower(borrower, true);
+
+        setupFees({
+            delegateOriginationFee:     500e6,
+            delegateServiceFee:         300e6,
+            delegateManagementFeeRate:  0.02e6,
+            platformOriginationFeeRate: 0.001e6,
+            platformServiceFeeRate:     0.31536e6,  // 10k after 1m seconds
+            platformManagementFeeRate:  0.08e6
+        });
+    }
+
+    function test_totalAssets_zeroTotalSupply() external {
+        assertEq(pool.totalAssets(), 0);
+    }
+
+    function test_totalAssets_singleDeposit() external {
+        depositLiquidity(lp1, 1_000e6);
+
+        assertEq(pool.totalAssets(), 1_000e6);
+    }
+
+    function test_totalAssets_singleLoanFunded() external {
+        depositLiquidity(lp1, 1_500_000e6);
+
+        loan = fundAndDrawdownLoan({
+            borrower:    borrower,
+            termDetails: [uint256(5 days), uint256(ONE_MONTH), uint256(3)],
+            amounts:     [uint256(0), uint256(1_500_000e6), uint256(1_000_000e6)],
+            rates:       [uint256(0.075e18), uint256(0), uint256(0), uint256(0)]
+        });
+
+        assertEq(fundsAsset.balanceOf(address(pool)), 0);  // Funds moved out of pool
+        assertEq(pool.totalAssets(),                  1_500_000e6);
+    }
+
+    function test_totalAssets_singleLoanFundedWithInterest() external {
+        depositLiquidity(lp1, 1_500_000e6);
+
+        loan = fundAndDrawdownLoan({
+            borrower:    borrower,
+            termDetails: [uint256(5 days), uint256(ONE_MONTH), uint256(3)],
+            amounts:     [uint256(0), uint256(1_000_000e6), uint256(1_000_000e6)],
+            rates:       [uint256(0.075e18), uint256(0), uint256(0), uint256(0)]
+        });
+
+        assertEq(fundsAsset.balanceOf(address(pool)), 500_000e6);  // Funds moved out of pool
+        assertEq(pool.totalAssets(),                  1_500_000e6);
+
+        vm.warp(start + ONE_MONTH);
+
+        // +------------+--------+--------+
+        // |    POOL    |   PD   |   MT   |
+        // +------------+--------+--------+
+        // |   500,000  |   500  |    250 |
+        // | +   6,250  | + 275  | +  550 | Interest and service fees paid
+        // | -     625  | + 125  | +  500 | Management fee distribution
+        // | = 505,625  | = 900  | = 1300 |
+        // +------------+--------+--------+
+        assertEq(pool.totalAssets(), 1_505_624_999999);  // Note: Rounding
+    }
+
+    function test_totalAssets_singleLoanFundedWithPayment() external {
+        depositLiquidity(lp1, 1_500_000e6);
+
+        loan = fundAndDrawdownLoan({
+            borrower:    borrower,
+            termDetails: [uint256(5 days), uint256(ONE_MONTH), uint256(3)],
+            amounts:     [uint256(0), uint256(1_000_000e6), uint256(1_000_000e6)],
+            rates:       [uint256(0.075e18), uint256(0), uint256(0), uint256(0)]
+        });
+
+        assertEq(fundsAsset.balanceOf(address(pool)), 500_000e6);  // Funds moved out of pool
+        assertEq(pool.totalAssets(),                  1_500_000e6);
+
+        /************************/
+        /*** Make 1st Payment ***/
+        /************************/
+
+        vm.warp(start + ONE_MONTH);
+        makePayment(loan);
+
+        // +------------+--------+--------+
+        // |    POOL    |   PD   |   MT   |
+        // +------------+--------+--------+
+        // |   500,000  |   500  |    250 |
+        // | +   6,250  | + 275  | +  550 | Interest and service fees paid
+        // | -     625  | + 125  | +  500 | Management fee distribution
+        // | = 505,625  | = 900  | = 1300 |
+        // +------------+--------+--------+
+        assertEq(pool.totalAssets(),                  1_505_625e6);
+        assertEq(fundsAsset.balanceOf(address(pool)), 505_625e6);
+    }
+
+}
