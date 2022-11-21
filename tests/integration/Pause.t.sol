e3db0b4b1680eeeb872077ef9bb2c4877ec27d9b
Joao Gabriel Carvalho <jg@jgcarv.com>
2022-11-21 18:54:39 -0300

    feat: Add remaining upgrade tests on global pause (SC-9716) (#154)
    * wip: add upgrade tests
    
    * feat: updated to latest modules
    
    * chore: update submodule
    
    * chore: update submodule
    
    * feat: reorder upgrade functions and add LM upgrade
    
    * feat: update submodules
    
    Co-authored-by: lucas-manuel <lucas@maple.finance>

--- tests/integration/Pause.t.sol
+++ tests/integration/Pause.t.sol
@@ -31,6 +31,33 @@ contract PauseTests is TestBaseWithAssertions {
     function setUp() public override {
         super.setUp();
 
+        // Create new implementations for upgrades
+        address liquidatorImplementationForUpgrade        = address(new Address());
+        address loanImplementationForUpgrade              = address(new Address());
+        address loanManagerImplementationForUpgrade       = address(new Address());
+        address poolManagerImplementationForUpgrade       = address(new Address());
+        address withdrawalManagerImplementationForUpgrade = address(new Address());
+
+        // Register implementation and upgrade paths
+        vm.startPrank(governor);
+
+        IMapleProxyFactory(liquidatorFactory).registerImplementation(2, liquidatorImplementationForUpgrade, address(0));
+        IMapleProxyFactory(liquidatorFactory).enableUpgradePath(1, 2, address(0));
+
+        IMapleProxyFactory(loanFactory).registerImplementation(2, loanImplementationForUpgrade, address(0));
+        IMapleProxyFactory(loanFactory).enableUpgradePath(1, 2, address(0));
+
+        IMapleProxyFactory(loanManagerFactory).registerImplementation(2, loanManagerImplementationForUpgrade, address(0));
+        IMapleProxyFactory(loanManagerFactory).enableUpgradePath(1, 2, address(0));
+
+        IMapleProxyFactory(poolManagerFactory).registerImplementation(2, poolManagerImplementationForUpgrade, address(0));
+        IMapleProxyFactory(poolManagerFactory).enableUpgradePath(1, 2, address(0));
+
+        IMapleProxyFactory(withdrawalManagerFactory).registerImplementation(2, withdrawalManagerImplementationForUpgrade, address(0));
+        IMapleProxyFactory(withdrawalManagerFactory).enableUpgradePath(1, 2, address(0));
+
+        vm.stopPrank();
+
         depositLiquidity({
             lp:        address(new Address()),
             liquidity: 1_500_000e6
@@ -65,6 +92,7 @@ contract PauseTests is TestBaseWithAssertions {
 
         globals.setValidPoolDelegate(pausePoolDelegate, true);
         globals.setProtocolPause(true);
+        globals.setValidBorrower(borrower, true);
 
         vm.stopPrank();
     }
@@ -76,7 +104,6 @@ contract PauseTests is TestBaseWithAssertions {
         /******************************************************************************************************************************/
 
         // Pool Manager
-
         bytes32 salt_ = keccak256(abi.encode(pausePoolDelegate));
         bytes memory arguments = IPoolManagerInitializer(poolManagerInitializer).encodeArguments(pausePoolDelegate, address(fundsAsset), 0, "Maple Pool", "MP");
 
@@ -85,7 +112,6 @@ contract PauseTests is TestBaseWithAssertions {
         IMapleProxyFactory(poolManagerFactory).createInstance(arguments, salt_);
 
         // Loan Manager
-
         arguments = ILoanManagerInitializer(loanManagerInitializer).encodeArguments(address(pool));
 
         vm.prank(address(deployer));
@@ -93,45 +119,86 @@ contract PauseTests is TestBaseWithAssertions {
         IMapleProxyFactory(loanManagerFactory).createInstance(arguments, salt_);
 
         // Withdrawal Manager
-
         arguments = IWithdrawalManagerInitializer(withdrawalManagerInitializer).encodeArguments(address(pool), 1 weeks, 1 days);
 
-        vm.expectRevert("MPF:CI:FAILED");  // NOTE: Protocol paused error will not bubble up
+        vm.expectRevert("MPF:PROTOCOL_PAUSED");
         vm.prank(address(deployer));
         IMapleProxyFactory(withdrawalManagerFactory).createInstance(arguments, salt_);
 
         // Liquidator
         arguments = LiquidatorInitializer(liquidatorInitializer).encodeArguments(address(loanManager), address(collateralAsset), address(fundsAsset));
 
-        vm.expectRevert("MPF:CI:FAILED");  // NOTE: Protocol paused error will not bubble up
+        vm.expectRevert("MPF:PROTOCOL_PAUSED");
         vm.prank(address(loanManager));
         IMapleProxyFactory(liquidatorFactory).createInstance(arguments, salt_);
 
-        // Loan
-        arguments = MapleLoanInitializer(loanInitializer).encodeArguments({
-            borrower_:    borrower,
-            feeManager_:  address(feeManager),
-            assets_:      [address(collateralAsset), address(fundsAsset)],
-            termDetails_: [uint256(5_000), uint256(1_000_000), uint256(3)],
-            amounts_:     [uint256(0), uint256(1_500_000e6), uint256(1_500_000e6)],
-            rates_:       [uint256(3.1536e18), uint256(0), uint256(0), uint256(0)],
-            fees_:        [nextDelegateOriginationFee, nextDelegateServiceFee]
-        });
+        /******************************************************************************************************************************/
+        /*** Liquidator                                                                                                             ***/
+        /******************************************************************************************************************************/
 
-        vm.expectRevert("MPF:CI:FAILED");  // NOTE: Protocol paused error will not bubble up
-        IMapleProxyFactory(loanFactory).createInstance(arguments, salt_);
+        vm.prank(governor);
+        vm.expectRevert("MPF:PROTOCOL_PAUSED");
+        liquidator.upgrade(2, "");
+
+        vm.expectRevert("LIQ:PROTOCOL_PAUSED");
+        liquidator.liquidatePortion(0, 0, "");
+
+
+        /******************************************************************************************************************************/
+        /*** Loan                                                                                                                   ***/
+        /******************************************************************************************************************************/
+
+        vm.prank(governor);
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.upgrade(2, "");
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.acceptBorrower();
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.closeLoan(0);
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.drawdownFunds(0, address(0));
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.makePayment(0);
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.postCollateral(0);
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.proposeNewTerms(address(0), 0, data);
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.removeCollateral(0, address(0));
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.returnFunds(0);
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.setPendingBorrower(address(0));
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.acceptLender();
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.rejectNewTerms(address(0), 0, data);
+
+        vm.expectRevert("L:PROTOCOL_PAUSED");
+        loan.skim(address(0), address(0));
+
+        /******************************************************************************************************************************/
+        /*** Loan Manager                                                                                                           ***/
+        /******************************************************************************************************************************/
+
+        vm.prank(governor);
+        vm.expectRevert("MPF:PROTOCOL_PAUSED");
+        loanManager.upgrade(2, "");
 
         /******************************************************************************************************************************/
         /*** Pool                                                                                                                   ***/
         /******************************************************************************************************************************/
-
-        // TODO: Uncomment after submodules are updated.
-        // vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
-        // pool.approve(address(0), 0);
-
-        // TODO: Uncomment after submodules are updated.
-        // vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
-        // pool.decreaseAllowance(address(0), 0);
 
         vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
         pool.deposit(0, address(0));
@@ -139,19 +206,11 @@ contract PauseTests is TestBaseWithAssertions {
         vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
         pool.depositWithPermit(0, address(0), 0, 0, 0, 0);
 
-        // TODO: Uncomment after submodules are updated.
-        // vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
-        // pool.increaseAllowance(address(0), 0);
-
         vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
         pool.mint(0, address(0));
 
         vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
         pool.mintWithPermit(0, address(0), 0, 0, 0, 0, 0);
-
-        // TODO: Uncomment after submodules are updated.
-        // vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
-        // pool.permit(address(0), address(0), 0, 0, 0, 0, 0);
 
         vm.expectRevert("PM:CC:PROTOCOL_PAUSED");
         pool.redeem(0, address(0), address(0));
@@ -244,10 +303,9 @@ contract PauseTests is TestBaseWithAssertions {
         vm.expectRevert("PM:PROTOCOL_PAUSED");
         poolManager.triggerDefault(address(0), address(0));
 
-        // TODO: Add upgrade path to test
-        // vm.prank(governor);
-        // vm.expectRevert("PM:PROTOCOL_PAUSED");
-        // poolManager.upgrade(2, "");
+        vm.prank(governor);
+        vm.expectRevert("MPF:PROTOCOL_PAUSED");
+        poolManager.upgrade(2, "");
 
         vm.expectRevert("PM:PROTOCOL_PAUSED");
         poolManager.withdrawCover(0, address(0));
@@ -256,67 +314,12 @@ contract PauseTests is TestBaseWithAssertions {
         /*** Withdrawal Manager                                                                                                     ***/
         /******************************************************************************************************************************/
 
-        // TODO: Add upgrade path to test
-        // vm.expectRevert("WM:PROTOCOL_PAUSED");
-        // withdrawalManager.upgrade(0, "");
+        vm.prank(governor);
+        vm.expectRevert("MPF:PROTOCOL_PAUSED");
+        withdrawalManager.upgrade(2, "");
 
         vm.expectRevert("WM:PROTOCOL_PAUSED");
         withdrawalManager.setExitConfig(0, 0);
-
-        /******************************************************************************************************************************/
-        /*** Loan                                                                                                                   ***/
-        /******************************************************************************************************************************/
-
-        // TODO: Add upgrade path to test
-        // vm.expectRevert("L:PROTOCOL_PAUSED");
-        // loan.upgrade(0, "");
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.acceptBorrower();
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.closeLoan(0);
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.drawdownFunds(0, address(0));
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.makePayment(0);
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.postCollateral(0);
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.proposeNewTerms(address(0), 0, data);
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.removeCollateral(0, address(0));
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.returnFunds(0);
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.setPendingBorrower(address(0));
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.acceptLender();
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.rejectNewTerms(address(0), 0, data);
-
-        vm.expectRevert("L:PROTOCOL_PAUSED");
-        loan.skim(address(0), address(0));
-
-        /******************************************************************************************************************************/
-        /*** Liquidator                                                                                                             ***/
-        /******************************************************************************************************************************/
-
-        // TODO: Add upgrade path to test
-        // vm.expectRevert("LIQ:PROTOCOL_PAUSED");
-        // liquidator.upgrade(0, "");
-
-        vm.expectRevert("LIQ:PROTOCOL_PAUSED");
-        liquidator.liquidatePortion(0, 0, "");
     }
 
 }
