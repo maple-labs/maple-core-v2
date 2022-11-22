f0c8b91c51fc679cb8f45436f9e6005b2bdeffd6
Farhaan <59924029+0xfarhaan@users.noreply.github.com>
2022-11-22 22:18:06 +0000

    feat: Update business sim to add accounted interest and fix import (#156)
    * feat: Update business sim to add accounted interest and fix import
    
    * style: Update alignment

--- contracts/loggers/BusinessSimLogger.sol
+++ contracts/loggers/BusinessSimLogger.sol
@@ -38,35 +38,37 @@ contract BusinessSimLogger is Logger {
     }
 
     function headers() external pure override returns (string[] memory headers_) {
-        headers_ = new string[](11);
+        headers_ = new string[](12);
 
-        headers_[0] = "Day";
-        headers_[1] = "Outstanding Principal";
-        headers_[2] = "Accrued Interest";
-        headers_[3] = "Unrealized Losses";
-        headers_[4] = "Cash";
-        headers_[5] = "Pool Value";
-        headers_[6] = "Pool Value w Unrealized Losses";
-        headers_[7] = "Cover Balance";
-        headers_[8] = "Treasury Balance";
-        headers_[9] = "Pool Delegate Balance";
-        headers_[10] = "Notes";
+        headers_[0]  = "Day";
+        headers_[1]  = "Outstanding Principal";
+        headers_[2]  = "Accrued Interest";
+        headers_[3]  = "Accounted Interest";
+        headers_[4]  = "Unrealized Losses";
+        headers_[5]  = "Cash";
+        headers_[6]  = "Pool Value";
+        headers_[7]  = "Pool Value w Unrealized Losses";
+        headers_[8]  = "Cover Balance";
+        headers_[9]  = "Treasury Balance";
+        headers_[10] = "Pool Delegate Balance";
+        headers_[11] = "Notes";
     }
 
     function output(string memory notes_) external view override returns (string[] memory values_) {
-        values_ = new string[](11);
+        values_ = new string[](12);
 
-        values_[0] = _formattedTime();
-        values_[1] = convertUintToSixDecimalString(loanManager.principalOut(),                                 1e6);
-        values_[2] = convertUintToSixDecimalString(loanManager.getAccruedInterest(),                           1e6);
-        values_[3] = convertUintToSixDecimalString(loanManager.unrealizedLosses(),                             1e6);
-        values_[4] = convertUintToSixDecimalString(asset.balanceOf(poolManager.pool()),                        1e6);
-        values_[5] = convertUintToSixDecimalString(poolManager.totalAssets(),                                  1e6);
-        values_[6] = convertUintToSixDecimalString(poolManager.totalAssets() - loanManager.unrealizedLosses(), 1e6);
-        values_[7] = convertUintToSixDecimalString(asset.balanceOf(poolManager.poolDelegateCover()),           1e6);
-        values_[8] = convertUintToSixDecimalString(asset.balanceOf(treasury),                                  1e6);
-        values_[9] = convertUintToSixDecimalString(asset.balanceOf(poolDelegate),                              1e6);
-        values_[10] = bytes(notes_).length == 0 ? NULL : notes_;
+        values_[0]  = _formattedTime();
+        values_[1]  = convertUintToSixDecimalString(loanManager.principalOut(),                                 1e6);
+        values_[2]  = convertUintToSixDecimalString(loanManager.getAccruedInterest(),                           1e6);
+        values_[3]  = convertUintToSixDecimalString(loanManager.accountedInterest(),                            1e6);
+        values_[4]  = convertUintToSixDecimalString(loanManager.unrealizedLosses(),                             1e6);
+        values_[5]  = convertUintToSixDecimalString(asset.balanceOf(poolManager.pool()),                        1e6);
+        values_[6]  = convertUintToSixDecimalString(poolManager.totalAssets(),                                  1e6);
+        values_[7]  = convertUintToSixDecimalString(poolManager.totalAssets() - loanManager.unrealizedLosses(), 1e6);
+        values_[8]  = convertUintToSixDecimalString(asset.balanceOf(poolManager.poolDelegateCover()),           1e6);
+        values_[9]  = convertUintToSixDecimalString(asset.balanceOf(treasury),                                  1e6);
+        values_[10] = convertUintToSixDecimalString(asset.balanceOf(poolDelegate),                              1e6);
+        values_[11] = bytes(notes_).length == 0 ? NULL : notes_;
     }
 
 }
