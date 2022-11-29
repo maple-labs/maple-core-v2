d068b99e38ed0cdf9c9b582957b9eac65286c05a
Farhaan <59924029+0xfarhaan@users.noreply.github.com>
2022-11-29 01:30:14 +0000

    feat: Add `transferOwnedPoolManager` integration (SC-10013) (#169)
    * feat: Add success case for transferring pool ownership
    
    * feat: Add additional success and failure mode tests for pool ownership
    
    * feat: Add additional failure mode tests
    
    * refactor: Make tests clearer and remove extra line

---
+++ tests/integration/TransferPoolOwnership.t.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.7;
+
+import { TestBaseWithAssertions } from "../../contracts/utilities/TestBaseWithAssertions.sol";
+
+import { Address, console } from "../../modules/contract-test-utils/contracts/test.sol";
+
+contract TransferPoolOwnershipTests is TestBaseWithAssertions {
+
+    address newPoolDelegate = address(new Address());
+
+    function test_setPendingPoolDelegate_notPD() external {
+        vm.expectRevert("PM:SPA:NOT_PD");
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        assertEq(poolManager.pendingPoolDelegate(), newPoolDelegate);
+    }
+
+    function test_acceptPendingPoolDelegate_notPendingPoolDelegate() external {
+        vm.prank(governor);
+        globals.setValidPoolDelegate(newPoolDelegate, true);
+
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.expectRevert("PM:APPD:NOT_PENDING_PD");
+        poolManager.acceptPendingPoolDelegate();
+
+        vm.prank(newPoolDelegate);
+        poolManager.acceptPendingPoolDelegate();
+
+        assertEq(poolManager.poolDelegate(),        newPoolDelegate);
+        assertEq(poolManager.pendingPoolDelegate(), address(0));
+
+        ( address poolManager_, bool isPoolDelegate_ ) = globals.poolDelegates(newPoolDelegate);
+
+        assertEq(poolManager_, address(poolManager));
+
+        assertTrue(isPoolDelegate_);
+    }
+
+    function test_transferOwnedPoolManager_notPoolManager() external {
+        vm.prank(governor);
+        globals.setValidPoolDelegate(newPoolDelegate, true);
+
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.prank(newPoolDelegate);
+        vm.expectRevert("MG:TOPM:NOT_AUTHORIZED");
+        globals.transferOwnedPoolManager(poolDelegate, newPoolDelegate);
+
+        vm.prank(newPoolDelegate);
+        poolManager.acceptPendingPoolDelegate();
+
+        assertEq(poolManager.poolDelegate(),        newPoolDelegate);
+        assertEq(poolManager.pendingPoolDelegate(), address(0));
+
+        ( address poolManager_, bool isPoolDelegate_ ) = globals.poolDelegates(newPoolDelegate);
+
+        assertEq(poolManager_, address(poolManager));
+
+        assertTrue(isPoolDelegate_);
+    }
+
+    function test_transferOwnedPoolManager_notValidPoolDelegate() external {
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.prank(newPoolDelegate);
+        vm.expectRevert("MG:TOPM:NOT_POOL_DELEGATE");
+        poolManager.acceptPendingPoolDelegate();
+
+        vm.prank(governor);
+        globals.setValidPoolDelegate(newPoolDelegate, true);
+
+        vm.prank(newPoolDelegate);
+        poolManager.acceptPendingPoolDelegate();
+
+        assertEq(poolManager.poolDelegate(),        newPoolDelegate);
+        assertEq(poolManager.pendingPoolDelegate(), address(0));
+
+        ( address poolManager_, bool isPoolDelegate_ ) = globals.poolDelegates(newPoolDelegate);
+
+        assertEq(poolManager_, address(poolManager));
+
+        assertTrue(isPoolDelegate_);
+    }
+
+    function test_transferOwnedPoolManager_alreadyPoolDelegate() external {
+        vm.prank(governor);
+        globals.setValidPoolDelegate(newPoolDelegate, true);
+
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.prank(newPoolDelegate);
+        poolManager.acceptPendingPoolDelegate();
+
+        assertEq(poolManager.poolDelegate(),        newPoolDelegate);
+        assertEq(poolManager.pendingPoolDelegate(), address(0));
+
+        ( address poolManager_, bool isPoolDelegate_ ) = globals.poolDelegates(newPoolDelegate);
+
+        assertEq(poolManager_, address(poolManager));
+
+        assertTrue(isPoolDelegate_);
+
+        vm.prank(newPoolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.prank(newPoolDelegate);
+        vm.expectRevert("MG:TOPM:ALREADY_OWNS");
+        poolManager.acceptPendingPoolDelegate();
+    }
+
+    function test_setPendingPoolDelegate() external {
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        assertEq(poolManager.pendingPoolDelegate(), newPoolDelegate);
+    }
+
+    function test_acceptPendingPoolDelegate() external {
+        vm.prank(governor);
+        globals.setValidPoolDelegate(newPoolDelegate, true);
+
+        vm.prank(poolDelegate);
+        poolManager.setPendingPoolDelegate(newPoolDelegate);
+
+        vm.prank(newPoolDelegate);
+        poolManager.acceptPendingPoolDelegate();
+
+        assertEq(poolManager.poolDelegate(),        newPoolDelegate);
+        assertEq(poolManager.pendingPoolDelegate(), address(0));
+
+        ( address poolManager_, bool isPoolDelegate_ ) = globals.poolDelegates(newPoolDelegate);
+
+        assertEq(poolManager_, address(poolManager));
+
+        assertTrue(isPoolDelegate_);
+    }
+    
+}
