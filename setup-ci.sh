23593305b56e40f1086c222416f2a9db8e9fec5b
0xfarhaan <59924029+0xfarhaan@users.noreply.github.com>
2025-03-21 17:45:00 +0400

    chore: fix CI

--- setup-ci.sh
+++ setup-ci.sh
@@ -1,7 +1,2 @@
-mkdir $HOME/.ssh
-touch $HOME/.ssh/id_rsa
-chmod 600 $HOME/.ssh/id_rsa
-
 git config --global url."git@github.com:".insteadOf "https://github.com/"
-
 git submodule update --init --recursive
